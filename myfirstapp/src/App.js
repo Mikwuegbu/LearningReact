b361753b6c7c4d010929b151541bc7eb0d0ebae8
Mikwuegbu <mstephen130@yahoo.com>
2023-08-23 08:42:34 +0100

    reactMovieApp

--- myfirstapp/src/App.js
+++ myfirstapp/src/App.js
@@ -1,13 +1,39 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+//import MovieCard from "./MovieCard";
+import SearchIcon from "./Search.svg";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
-  const name = "John";
-  return (
-    <div className="App">
-      <h1>Hello React!! I am {name}</h1>
 
-    </div>
-  );
-}
+
+    // useEffect(() => {
+    //     searchMovies("Batman");
+    // }, []);
+
+    // const searchMovies = async (title) => {
+    //     const response = await fetch(`${API_URL}&s=${title}`);
+    //     const data = await response.json();
+
+    // };
+
+    return (
+        <div className="app">
+            <h1>Netflix and Chill</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    onChange={() => { }}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => { }} />
+            </div>
+        </div>
+    );
+};
 
 export default App;
